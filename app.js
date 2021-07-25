80e4ed009dcb23a2c2a78f8dcefc45a60dff98c7
doctone <samjojames@gmail.com>
2021-07-25 19:19:37 +0100

    updated task list

---
+++ app.js
@@ -0,0 +1,18 @@
+// export {generateHighItem, generateLowItem}
+
+// function generateLowItem() {
+//     const value = document.getElementById("item").value;
+//     if (value == '') {
+//         return
+//     }
+//     document.getElementById("low").innerHTML += `<button class="btn btn-success">${value}</button>`
+//     document.getElementById("item").value = ''
+// }
+// function generateHighItem() {
+//     const value = document.getElementById("item").value;
+//     if (value == '') {
+//         return
+//     }
+//     document.getElementById("high").innerHTML += `<button class="btn btn-danger">${value}</button>`
+//     document.getElementById("item").value = ''
+// }
